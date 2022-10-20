3c0b7c36d6fdee989554b06de074c39ad3cff123
cheboi <moscheb96@gmail.com>
2022-10-20 14:26:32 +0300

    delete schema

--- src/docs/todos/delete-todo.js
+++ src/docs/todos/delete-todo.js
@@ -1,3 +1,30 @@
 module.exports = {
-    
-}
+    delete:{
+        tags:["Todo CRUD operations"],
+        description: "Delete a todo",
+        operationId: "deleteTodo",
+        parameters: [
+            {
+                name: "id",
+                in: "path",
+                shema: {
+                    $ref: "#/components/schemas/id",
+                },
+                required: true,
+                description: "Deleting a done todo",
+            },
+        ],
+
+        responses:{
+            200: {
+                description: "Todo deleted successfully",
+            },
+            400: {
+                description: "Todo not found",
+            },
+            500: {
+                description: "Server Error",
+            },
+        },
+    },
+};
