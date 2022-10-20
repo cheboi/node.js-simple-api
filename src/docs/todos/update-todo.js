9cdce73c7894e7365cc7cb5e8885d4ef71112e27
cheboi <moscheb96@gmail.com>
2022-10-20 13:45:53 +0300

    update modal

--- src/docs/todos/update-todo.js
+++ src/docs/todos/update-todo.js
@@ -1,3 +1,32 @@
 module.exports = {
-    
+    put:{
+        tags: ["Todo CRUD operations"], 
+        description: "Update todo", 
+        operationId: "updateTodo", 
+        parameters: [
+            {
+                name: "id", 
+                in: "path", 
+                schema: {
+                    $ref: "#/components/schemas/id",
+                },
+                required: true,
+                description: "Id of todo to be updated",
+            }
+        ],
+
+        responses:{
+            200: {
+                description: "Todo updated successfully",
+            },
+
+            400: {
+                description: "Todo not found",
+            },
+
+            500:{
+                description: "Server Error",
+            },
+        },
+    },
 }
